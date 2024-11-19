04788cd6aa83407c496fcae0665cf796aea84d86
SarahBiscuit <150682385+SarahBiscuit@users.noreply.github.com>
2024-11-19 22:14:36 +0000

    Table basic structure set up
    placeholder content added

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /*PLAN FOR GETTING TO DO LIST JAVASCRIPT TO WORK (check against online examples but these are my ideas)
 
 1. Create an array to store the user's input answers for each question.
-2. Set an eventListener so that each time the submit button is clicked the user input is added to the arrays using .push().
+2. Set an eventListener so that each time the submit button is clicked the user input is added to the arrays using .push().  Is this something to 
+create an object for, so that each to do list entry becomes it's own object?  I think that that is the logical thing to do.
 3. If there is no content that the user has added, set it so that .push(); will not Work.
 4. Set up .push so that the array elements are used to populate a table on a new page of my website.
 5. Need to find a method to remove any to do list item of the user's choosing from the table if needed (delete button on the relevant row?).  
