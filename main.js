66bd81110c55b94568d3f50aee7a3b7bd49a971e
SarahBiscuit <scfletcher2000@hotmail.com>
2025-07-20 17:43:27 +0100

    Initial work to add a database
    Database created in pgAdmin and start of creating database connection in Node

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ import express from "express";
 const app = express();
 import bodyParser from "body-parser";
 import { v4 as uuidv4 } from "uuid";
+import pg from "pg";
+
+//Create the database DONE    
+//Create the database connection in this file
+/*When i select the data field from the database for the table, I will need to use the following SQL code
+SELECT TO_CHAR(your_date_column, 'DD/MM/YYYY') AS formatted_date FROM your_table;
+This is because I want to format it as DD/MM/YYYY and this is not how SQL stores it in the database*/
+//Amend my routes to handle the new database structure - in the main-js and index.ejs
+//Looking at putting an API between the back-end and the database to handle the database requests.  Need to do some research on this.
+//Consider adding an edit route to update items in the database (would, however, also involve adding an edit button in the UI)
+//Will I still need uuid when I am using a database?
+//DATABASE PLAN - four columns - id(serial primary key) (which is why I am thinkign I won't need uuid), deadline, description and notes.
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "toDoListWebsite",
+  password: "INSERT MY PASSWORD HERE",
+  port: 5432,
+});
+db.connect();
 
 const newToDoListItem = [];
 
