d814737c764d264763aa2542cf354da4d4be11bc
SarahBiscuit <scfletcher2000@hotmail.com>
2025-07-21 20:36:14 +0100

    imported dotenv
    Imported dotenv

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 import bodyParser from "body-parser";
 import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = new pg.Client({
   user: process.env.DB_USER,
